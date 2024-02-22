97557827fb5b6304458bf14ded483c21a3091508
bundumunu <andriagiorgadze589@gmail.com>
2024-02-22 19:00:03 +0400

    commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,39 @@
+name="andria"
+surname="giorgadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზეგამოსატანი ობიექტი 
+
+name="andria" #ეს არის str (string) ტიპის ცვლადი
+age=11 #ეს არის int(integer) მთელი რიცხვი 
+height=172.2 #ეს არის float ცვლადი (ათწილადი)  #boolean (bool) ტიპის ცვლადი
+
+knows_programming=True 
+#true ან false  is_ugly=false
+#snakecase(უბრალოდ წერის სტილი სტანდარტულად) 
+is_ugly=False #ჯავასკრიპტული camelcase
+
+
+
+#print(name + " " + surname )
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name+age)
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name+ " " + str(age))
+
+
+#მე ვარ ანდრია 11 წლის ჩემი გვარია გიორგაძე.
+
+
+
+
+
+
+
---
+++ day 4/day4.py
@@ -0,0 +1,4 @@
+name="andria"
+surname="giorgadze"
+age=11
+print("my name is", andria, "my surname is", giorgadze, "my age is", 11)
