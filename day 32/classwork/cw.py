b49149e6dd7d03f8bbf96877340007a2fec6a540
bundumunu <andriagiorgadze589@gmail.com>
2024-05-26 12:29:07 +0400

    commit

---
+++ day 32/classwork/cw.py
@@ -0,0 +1,22 @@
+# name = "gabriel"
+# surname = "molodini"
+# age = 26
+
+
+# მონაცემთა ტიპები
+# str
+# int
+# float 
+# bool
+# list
+
+group7_students = ['gabrieli', 'mate', 'daviti', 'merabi', 'lazare', 'mariami', 'nikolozi', 'gio', 'aleqsandre', 'ugo', 'andria']
+print(group7_students)
+print(type(group7_students))
+
+cars = ['nissan' 'bmw' 'lamborgini' 'buggati' 'ჟიგული' ]
+print(group7_students[8])
+
+
+
+# list - სია 
