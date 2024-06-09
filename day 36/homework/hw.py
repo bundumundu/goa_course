82dea963bd6f64b99e980949300be764d0ace1e7
bundumunu <andriagiorgadze589@gmail.com>
2024-06-09 21:15:03 +0400

    commit

---
+++ day 36/homework/hw.py
@@ -0,0 +1,12 @@
+good_footballers = ["messi", "ronaldo", "neymar jr", "kvara", "maradona", "pele", "iniesta", "del piero"]
+#შევქმენით ლისტი
+
+
+#print(good_footballers[3:6])
+# ახლა გამოვიყვანეთ ლისტის შუა სამი წევრი
+
+#print(good_footballers[:])
+# ახლა გამოვიყვანეთ ლისტის ყველა ელემენტი
+
+#print(good_footballers[:3])
+# ახლა გამოვიყვანეთ ლისტის პირველი 3 ელემენტი
