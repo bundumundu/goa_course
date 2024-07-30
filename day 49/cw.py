4dd0bf91250b2bcfc65ce519bae2958ea0b09bb6
bundumunu <andriagiorgadze589@gmail.com>
2024-07-30 16:24:57 +0400

    commit

---
+++ day 49/cw.py
@@ -0,0 +1,8 @@
+#student random generator
+# პროგრამას უნდა გადაეცეს მოსწავლეების სახელებით სავე სია, პროგრამამ რანდომულათ სამ სამი ბავშვი უნდა გაანაწილოს გუნდებად 
+
+group7_students = ["merab gaganidze" "mate qurcikidze" "nikoloz tsiklauri" "aleqsandre chanqseliani" "gio gvardzela" "lazare iakobadze" "andria giorgadze" "merab chukruxidze" "demetre devadze" "tazo edisherashvili" "mariam gvilava" "ugo" ]
+
+#group7_students.append("andria")
+
+print(group7_students[1])
