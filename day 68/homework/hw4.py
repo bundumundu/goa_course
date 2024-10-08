1a7bf6537fe8225a602bb0d51d97e1236a472c4c
bundumundu <andriagiorgadze589@gmail.com>
2024-10-08 15:28:58 +0400

    commit

---
+++ day 68/homework/hw4.py
@@ -0,0 +1,7 @@
+#4) შექმენი ფუნქცია, რომელიც შემოატანინებს მომხარებელს ორ რიცხვს და დააბრუნებს მათ ჯამს.
+
+input_1 = int(input("enter a num: "))
+input_2 = int(input("enter a num: "))
+def inputs(input_1, input_2):
+    return input_1 + input_2
+print(inputs(input_1, input_2))
