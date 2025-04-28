9e1e2f74f9398f277ec3d25c9671f0bcbeae7875
bundumundu <andriagiorgadze589@gmail.com>
2025-04-28 17:10:02 +0400

    commit

---
+++ day 123/classwork/cw2.py
@@ -0,0 +1,11 @@
+# სახელების სია
+names = ["ნიკა", "ანა", "გიორგი", "თამარი", "ბექა"]
+
+# ანბანის მიხედვით დალაგება
+sorted_names = sorted(names)
+
+# სიის შებრუნება Indexing-ის საშუალებით
+reversed_names = sorted_names[::-1]
+
+# შედეგის გამოტანა
+print(reversed_names)
---
+++ day 123/classwork/cw4.py
@@ -0,0 +1,8 @@
+# რიცხვების სია ზრდადობის მიხედვით
+numbers = [5, 12, 18, 23, 29, 34, 40, 47, 53, 60]
+
+# სიის დალაგება კლებადობის მიხედვით
+sorted_descending = sorted(numbers, reverse=True)
+
+# შედეგის გამოტანა
+print(sorted_descending)
