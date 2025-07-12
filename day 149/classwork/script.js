aa654d5351823218bd215bff753fd6a4f9f9d9bd
bundumundu <andriagiorgadze589@gmail.com>
2025-07-12 21:05:44 +0400

    commit

---
+++ day 149/classwork/script.js
@@ -0,0 +1,8 @@
+let name = 'lebron'
+const surname = 'james'
+const age = 40
+
+
+console.log(age)
+console.log(name)
+console.log(surname)
---
+++ day 149/classwork/script2.js
@@ -0,0 +1,4 @@
+let age = 25; 
+let age2 = 10;
+console.log(age + age2);         
+console.log(typeof age);
