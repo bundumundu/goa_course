e016f3b12aeaf5ec6ea8e887dfe86334075c9d9a
bundumundu <andriagiorgadze589@gmail.com>
2025-07-16 18:59:16 +0400

    commit

---
+++ day 149/homework/script.js
@@ -0,0 +1,41 @@
+//1 ოჯახის წევრების სახელები
+let mom = "ნანა";
+let dad = "გიორგი";
+let sister = "მარიამი";
+console.log("ჩემი დედის სახელი არის " + mom + ", მამის — " + dad + ", და დის — " + sister + ".");
+
+//2 ორი რიცხვი და მათემატიკური მოქმედებები
+let num1 = 10;
+let num2 = 3;
+console.log("ჯამი:", num1 + num2);
+console.log("სხვაობა:", num1 - num2);
+console.log("გამრავლება:", num1 * num2);
+console.log("გაყოფა:", num1 / num2);
+console.log("ნაშთი:", num1 % num2);
+
+//3 კომენტარები ცვლადებზე:
+/*
+ცვლადის შექმნა შეიძლება 3 გზით: var, let, const.
+let — ცვლადი, რომელიც შეიძლება შეიცვალოს.
+const — ცვლადი, რომლის შეცვლა აღარ შეიძლება.
+*/
+
+// 4ჩემი მონაცემები
+const name = "ანდრია";
+const lastname = "გიორგაძე";
+const age = 15;
+const height = 175;
+const favSport = "ფეხბურთი";
+console.log("მე ვარ " + name + " " + lastname + ", ვარ " + age + " წლის, სიმაღლით " + height + " სმ და ჩემი საყვარელი სპორტია " + favSport + ".");
+
+
+//მეხუთე ვერ გავაკეთე
+
+//6  ცვლადი და მათი ტიპების შემოწმება
+let string = "მოგესალმებით";
+let float = 3.14;
+let integer = 25;
+
+console.log(typeof string);    
+console.log(typeof float); 
+console.log(typeof integer);
