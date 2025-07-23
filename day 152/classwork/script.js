37fca895946d47d86c57d07b501c2952c83ac8a1
bundumundu <andriagiorgadze589@gmail.com>
2025-07-23 21:10:34 +0400

    commit

---
+++ day 152/classwork/script.js
@@ -0,0 +1,12 @@
+const correctname = "andria" 
+const correctpassword = "1001"
+
+const name = prompt("შეიყვანეთ სახელი:");
+const password = prompt("შეიყვანეთ პაროლი:");
+
+        
+if (name === correctname, password === correctpassword) {
+    alert("მომხმარებელი წარმატებით შევიდა საიტზე.");
+} else {
+    alert("სახელი ან პაროლი არასწორია.");
+}
